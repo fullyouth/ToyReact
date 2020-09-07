031835ccf2bdc3839cb02d81bc6ddc55d8f582a1
zhanghaoqi <fullyouth@163.com>
2020-09-07 10:40:10 +0800

    component完成

---
+++ main.js
@@ -0,0 +1,16 @@
+import { createElement, Component, render } from './toy-react'
+
+class MyCompoment extends Component {
+  render() {
+    return (<div>
+      <h1>MyCompoment</h1>
+      {this.children}
+    </div>)
+  }
+}
+
+render(<MyCompoment id="a" class="c">
+  <div>abc</div>
+  <div>123</div>
+  <div>zhq</div>
+</MyCompoment>, document.body)
